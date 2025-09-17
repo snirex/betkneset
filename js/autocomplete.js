c1bbc76215b994cd72d0153281c1fd503e62557e
Snir Elgabsi <snirex@users.noreply.github.com>
2025-09-17 17:24:12 +0300

    Update autocomplete.js

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -4,7 +4,7 @@ const STORAGE_KEY = "autocompleteData"; // מפתח מרכזי ל-localStorage
 // ---------- פונקציות ניהול נתונים ----------
 function loadData() {
     let data = localStorage.getItem(STORAGE_KEY);
-    return data ? JSON.parse(data) : { name: [], mitzvah: [], paymentType: [] };
+    return data ? JSON.parse(data) : {};
 }
 
 function saveData(data) {
@@ -16,6 +16,7 @@ function addToAutocomplete(type, value) {
     value = value.trim();
     if (!value) return;
     let data = loadData();
+    if (!data[type]) data[type] = [];
     const idx = data[type].indexOf(value);
     if (idx !== -1) data[type].splice(idx, 1); // אם קיים, הסר מהמיקום הקודם
     data[type].unshift(value); // הוסף למעלה
@@ -28,40 +29,9 @@ function getAutocompleteList(type) {
     return data[type] || [];
 }
 
-// ---------- פונקציות לניהול רשימות בדף ניהול ----------
-function renderList(type, listId) {
-    const data = loadData();
-    const $list = $(listId);
-    $list.empty();
-    data[type].forEach((item, index) => {
-        $list.append(`<li>${item} <button onclick="removeItem('${type}', ${index})">❌</button></li>`);
-    });
-}
-
-function addItem(type, inputId, listId) {
-    const val = $(inputId).val().trim();
-    if (!val) return;
-    let data = loadData();
-    if (!data[type].includes(val)) {
-        data[type].push(val);
-        saveData(data);
-        renderList(type, listId);
-    }
-    $(inputId).val("");
-}
-
-function removeItem(type, index) {
-    const data = loadData();
-    data[type].splice(index, 1);
-    saveData(data);
-    if (type === "name") renderList(type, "#listNames");
-    if (type === "mitzvah") renderList(type, "#listMitzvot");
-    if (type === "paymentType") renderList(type, "#listPayments");
-}
-
 // ---------- AUTOCOMPLETE GMAIL-STYLE ----------
 function initAutocomplete() {
-    $("#addPaymentModal input.autocomplete").each(function () {
+    $("input[data-autocomplete-type]").each(function () {
         const type = $(this).data("autocomplete-type");
         $(this).autocomplete({
             source: function (request, response) {
@@ -90,17 +60,5 @@ function initAutocomplete() {
 
 // ---------- INIT PAGE ----------
 $(function () {
-    // דף ניהול
-    if ($("#addMisPallel").length) {
-        renderList("name", "#listNames");
-        renderList("mitzvah", "#listMitzvot");
-        renderList("paymentType", "#listPayments");
-
-        $("#addMisPallel").on("click", () => addItem("name", "#newMisPallel", "#listNames"));
-        $("#addMitzva").on("click", () => addItem("mitzvah", "#newMitzva", "#listMitzvot"));
-        $("#addPayment").on("click", () => addItem("paymentType", "#newPayment", "#listPayments"));
-    }
-
-    // דף index.html - אתחול autocomplete לכל השדות במודאל
-    if ($("#addPaymentModal").length) initAutocomplete();
+    initAutocomplete();
 });
